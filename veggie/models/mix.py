eea88d5e985fe5820eaec8a7ed8c64ee78fb4957
Ignacio Buioli <ibuioli@gmail.com>
2021-11-09 10:19:44 -0300

    fix: mix

--- veggie/models/mix.py
+++ veggie/models/mix.py
@@ -222,7 +222,7 @@ class ReportStockPickingQs(models.AbstractModel):
                     stock_name_count = len(rec.origin)
                     stock_name_count = stock_name_count - 3 
                     stock_name= rec.origin[:stock_name_count]
-                    code_name = rec.origin[stock_name_count:] + "C"
+                    code_name = rec.origin[stock_name_count:] + "M"
 
                     date = {
                         'date_order': date_order,
