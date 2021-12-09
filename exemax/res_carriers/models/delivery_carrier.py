08f25cb37bb946920bb9cdd926fd25360be9b782
Gabriela Riquelme <gabriela@exemax.com>
2021-12-09 13:55:54 -0300

    carriers and contacts

--- exemax/res_carriers/models/delivery_carrier.py
+++ exemax/res_carriers/models/delivery_carrier.py
@@ -14,6 +14,14 @@ class res_Carrier(models.Model):
     _order = 'sequence, id'
 
     name = fields.Char('Expreso', required=True, translate=True)
+    razon_social = fields.Char('Razón Social')
+    direccion = fields.Char('Dirección')
+    ciudad = fields.Char('Ciudad')
+    telefono = fields.Char('Teléfono')
+    movil = fields.Char('Móvil')
+    email =fields.Char('Correo electrónico')
     active = fields.Boolean(default=True)
     sequence = fields.Integer(help="Determine the display order", default=10)
 
+    country_ids = fields.Many2many('res.country', 'delivery_carrier_country_rel', 'carrier_id', 'country_id', 'Countries')
+    state_ids = fields.Many2many('res.country.state', 'delivery_carrier_state_rel', 'carrier_id', 'state_id', 'States')
