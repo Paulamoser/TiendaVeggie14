506c55636091682fcd6035f86142b3d91cce0877
Gabriela Riquelme <gabriela@exemax.com>
2021-12-21 22:08:37 -0300

    payment_fix

--- odoo-argentina/account_payment_fix/models/account_payment.py
+++ odoo-argentina/account_payment_fix/models/account_payment.py
@@ -268,8 +268,8 @@ class AccountPayment(models.Model):
                 self.move_id._set_next_sequence()
 
                 #moves= self.env['account.move'].search_count([('payment_id','=', str(self.payment_group_id))])
-                moves= self.env['account.move'].search([('state','=', 'draft'),('payment_id', '>', '0')])
-                _logger.info('payment_group_ids:' + str(moves.payment_id.id))
+                moves= self.env['account.move'].search([('state','=', 'draft')])
+                _logger.info('payment_group_ids:' + str(moves.payment_id.payment_group_id ))
 
             self.currency_id = (
                 self.journal_id.currency_id or self.company_id.currency_id)
