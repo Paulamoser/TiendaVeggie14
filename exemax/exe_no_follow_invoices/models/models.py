347aebd73e599f1f7c07cc8679685865dd9d5b2b
Ignacio Buioli <ibuioli@gmail.com>
2022-05-05 11:02:39 -0300

    fix: revert

--- exemax/exe_no_follow_invoices/models/models.py
+++ exemax/exe_no_follow_invoices/models/models.py
@@ -5,6 +5,6 @@ from odoo import models, fields, api
 class AccountMoveNew(models.Model):
 	_inherit = "account.move"
 
-	def create(self, vals):
-		res = super(AccountMoveNew, self.with_context(tracking_disable=True)).create(vals)
-		return res
+	#def create(self, vals):
+	#	res = super(AccountMoveNew, self.with_context(tracking_disable=True)).create(vals)
+	#	return res
