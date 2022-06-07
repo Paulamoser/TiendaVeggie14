20ac803d2700f3fe0b94c0fc8cdc743267ba9fdb
Ignacio Buioli <ibuioli@gmail.com>
2022-06-07 16:09:47 -0300

    feat: new products style

--- veggie/static/src/js/products.js
+++ veggie/static/src/js/products.js
@@ -6,6 +6,27 @@ odoo.define('veggie.snippet_product', ['web.ajax'], function (require) {
     $(document).ready(function () {
         console.log('Veggie Website ON');
         var container_ingresos = document.getElementById("veggie_ingresos");
+        var container_ingresos_2 = document.getElementById("veggie_ingresos_2");
+
+        if (container_ingresos_2) {
+            container_ingresos_2.innerHTML = "";
+            ajax.jsonRpc('/get_products/ingresos','call', {}).then(function(data) {
+                console.log(data);
+                container_ingresos_2.innerHTML = "";
+                for (var i = 0; i < data.length; i++) {
+                    if (i === 0) {
+                        container_ingresos_2.innerHTML += '\
+                                            <div class="col-12 mb-3 p_ingreso">\
+                                                <a href="/shop/product/' + data[i].id +'" class="d-block col-12 col-lg-10 text-center pt-3 pl-3 pr-3 m-auto">\
+                                                    <img class="img-fluid" src="/web/image/product.template/'+data[i].id+'/image_256" loading="lazy" width="256" height="256" />\
+                                                    <h6 class="mt-3 text-center pb-1 mb-1">' + data[i].name + '</h6>\
+                                                    <h6 class="text-center pb-1 mb-1">$ ' + Math.round(data[i].combination.price) + '</h6>\
+                                                </a>\
+                                            </div>';
+                    }
+                }
+            });
+        }
 
         if (container_ingresos) {
             container_ingresos.innerHTML = "";
@@ -13,15 +34,16 @@ odoo.define('veggie.snippet_product', ['web.ajax'], function (require) {
                 console.log(data);
                 container_ingresos.innerHTML = "";
                 for (var i = 0; i < data.length; i++) {
-                    container_ingresos.innerHTML += '\
-                                            <div class="col-6 col-sm-6 col-md-4 col-lg-2 mb-3">\
+                    if (i !== 0) {
+                        container_ingresos.innerHTML += '\
+                                            <div class="col-3 col-sm-3 col-md-3 col-lg-3 mb-3 p_ingreso">\
                                                 <a href="/shop/product/' + data[i].id +'" class="d-block col-12 col-lg-10 text-center pt-3 pl-3 pr-3 m-auto">\
                                                     <img class="img-fluid" src="/web/image/product.template/'+data[i].id+'/image_256" loading="lazy" width="256" height="256" />\
-                                                    <h6 class="mt-3 text-left pb-1 mb-1">' + data[i].name + '</h6>\
-                                                    <h6 class="text-left pb-1 mb-1">$ ' + Math.round(data[i].combination.price) + '</h6>\
-                                                    <small class="text-primary text-left float-left">Comprar ahora</small>\
+                                                    <h6 class="mt-3 text-center pb-1 mb-1">' + data[i].name + '</h6>\
+                                                    <h6 class="text-center pb-1 mb-1">$ ' + Math.round(data[i].combination.price) + '</h6>\
                                                 </a>\
                                             </div>';
+                    }
                 }
             });
         }
@@ -56,8 +78,8 @@ odoo.define('veggie.snippet_product', ['web.ajax'], function (require) {
                 container_destacados.innerHTML = "";
                 for (var i = 0; i < data.length; i++) {
                     container_destacados.innerHTML += '\
-                                            <div class="col-6 col-sm-6 col-md-8 col-lg-8 mb-3"></div>\
-                                            <div class="col-6 col-sm-6 col-md-4 col-lg-4 mb-3 p_featured">\
+                                            <div class="d-none d-sm-block col-sm-6 col-md-8 col-lg-8 mb-3"></div>\
+                                            <div class="col-12 col-sm-6 col-md-4 col-lg-4 mb-3 p_featured">\
                                                 <a href="/shop/product/' + data[i].id +'" class="d-block col-12 col-lg-10 text-center pt-3 pl-3 pr-3 m-auto">\
                                                     <img class="img-fluid" src="/web/image/product.template/'+data[i].id+'/image_256" loading="lazy" width="256" height="256" />\
                                                     <h6 class="mt-3 text-center pb-1 mb-1">' + data[i].name + '</h6>\
