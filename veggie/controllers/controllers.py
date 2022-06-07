4f55b7647932fc9fad7822edc96aad7ad4319621
Ignacio Buioli <ibuioli@gmail.com>
2022-06-07 15:56:03 -0300

    feat: style for feature

--- veggie/controllers/controllers.py
+++ veggie/controllers/controllers.py
@@ -72,7 +72,7 @@ class WebsiteForm(WebsiteForm):
 
     @http.route(['/get_products/featured'], type='json', auth="public", website=True)
     def get_products_feature(self):
-        products = http.request.env['product.template'].sudo().search([('website_published','=',True),('product_featured','=',True)], limit=6, order='website_sequence asc')
+        products = http.request.env['product.template'].sudo().search([('website_published','=',True),('product_featured','=',True)], limit=1, order='website_sequence asc')
         pricelist = http.request.env['product.pricelist'].sudo().search([('id','=',1)], limit=1)
         p = []
 
