c48ae0dda89aa39c6890f75fc586eb615c581a5f
Ignacio Buioli <ibuioli@gmail.com>
2022-06-23 12:24:45 -0300

    Revert "fix: size img"
    This reverts commit 041243623ae2f6d245053c27a882891a49e14d0c.

--- veggie/static/src/js/products.js
+++ veggie/static/src/js/products.js
@@ -16,9 +16,9 @@ odoo.define('veggie.snippet_product', ['web.ajax'], function (require) {
                 for (var i = 0; i < data.length; i++) {
                     if (i === 0) {
                         container_ingresos_2.innerHTML += '\
-                                            <div class="col-12 mb-3 p_ingreso" style="color:#222; padding-top:60px">\
+                                            <div class="col-14 mb-3 p_ingreso" style="color:#222; padding-top:60px">\
                                                 <a href="/shop/product/' + data[i].id +'" class="d-block col-12 col-lg-10 text-center pt-3 pl-3 pr-3 m-auto" style="color:#222;">\
-                                                    <img class="img-fluid" src="/web/image/product.template/'+data[i].id+'/image_256" loading="lazy" width="350" height="350" />\
+                                                    <img class="img-fluid" src="/web/image/product.template/'+data[i].id+'/image_256" loading="lazy" width="256" height="256" />\
                                                     <h6 class="mt-3 text-center pb-1 mb-1">' + data[i].name + '</h6>\
                                                     <h6 class="text-center pb-1 mb-1">$ ' + Math.round(data[i].combination.price) + '</h6>\
                                                 </a>\
